46267cfb2fc014b8c3cbee53b270c24478fd865c
deathhunteryt <tiagomaiapereira900@gmail.com>
2021-03-07 23:59:48 +0000

    Adicionei um "Guarda" e comecei no Script de Patrulha e deteção (IA)

---
+++ Assets/Scripts/Guard AI.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardAI : MonoBehaviour
+{
+    public Transform player;
+    public bool playerSeen;
+    public float AIMoveSpeed = 5f;
+    public float damping;
+    public Collider view;
+
+    public Transform[] navPoint;
+    public UnityEngine.AI.NavMeshAgent agent;
+    public int destPoint = 0;
+    public Transform goal;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.destination = goal.position;
+
+        agent.autoBraking = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerSeen == true)
+        {
+            LookAtPlayer();
+            Chase();
+        }
+        else
+        {
+            GoToNextPoint();
+        }
+        if (agent.remainingDistance < .5f)
+        {
+            GoToNextPoint();
+        }
+
+    }
+
+    void GoToNextPoint()
+    {
+        if(navPoint.Length == 0)
+        {
+            agent.destination = navPoint[destPoint].position;
+            destPoint = (destPoint + 1) % navPoint.Length;
+        }
+    }
+
+    void OnTriggerEnter(Collider view)
+    {
+        playerSeen = true;
+    }
+
+    void LookAtPlayer() //for testing purposes
+    {
+        transform.LookAt(player);
+        
+    }
+
+    void Chase()
+    {
+        transform.Translate(Vector3.forward * AIMoveSpeed * Time.deltaTime);
+    }
+}
