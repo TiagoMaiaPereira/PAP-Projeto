b2c93c6b94d5418506d0d3d5c037dc90b3d16a72
Tiago Pereira <tiagomaiapereira900@gmail.com>
2021-05-13 13:11:32 +0100

    Continuação da configuração do menu de opções

--- Assets/Scripts/Menus & Buttons/MainMenu.cs
+++ Assets/Scripts/Menus & Buttons/MainMenu.cs
@@ -2,9 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public AudioMixer audioMixer;
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+    }
+
+    //Menu Principal
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -14,5 +27,21 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("Quit!");
         Application.Quit();
+    }
+
+    //options menu
+    public void SetVolume (float volume) //slide do volume
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
     }
 }
