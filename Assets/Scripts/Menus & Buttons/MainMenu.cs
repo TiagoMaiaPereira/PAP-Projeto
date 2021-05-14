b4fb00f4d1ccb8adf301efad5b5861d6982f7f67
Tiago Pereira <tiagomaiapereira900@gmail.com>
2021-05-14 12:24:33 +0100

    Menu de opções concluído, música adicionada (terminar os níveis para que se possa fazer o Lvl Select)

--- Assets/Scripts/Menus & Buttons/MainMenu.cs
+++ Assets/Scripts/Menus & Buttons/MainMenu.cs
@@ -10,11 +10,33 @@ public class MainMenu : MonoBehaviour
 
     public AudioMixer audioMixer;
 
+    public Dropdown resolutionDropdown;
+
     Resolution[] resolutions;
 
     void Start()
     {
         resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i <resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
     //Menu Principal
@@ -43,5 +65,11 @@ public class MainMenu : MonoBehaviour
     public void SetFullScreen(bool isFullScreen)
     {
         Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
