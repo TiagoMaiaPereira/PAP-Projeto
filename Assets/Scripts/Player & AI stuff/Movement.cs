e3f59d2339fa924bdcca9fac94fad26c1768159e
Tiago Pereira <tiagomaiapereira900@gmail.com>
2021-05-18 14:41:55 +0100

    Removi o salto do jogador (não achei necessário)

--- Assets/Scripts/Player & AI stuff/Movement.cs
+++ Assets/Scripts/Player & AI stuff/Movement.cs
@@ -10,12 +10,6 @@ public class Movement : MonoBehaviour
 
     public float speed = 6f; //Velocidade do jogador
 
-    public float gravity = 9.81f; //força da gravidade aplicada à entidade para que volte ao chão
-
-    public float jumpSpeed = 5f; //Indica a alturta do Salto
-
-    private float directionY; //para isolar o eixo Y
-
     public Transform player;
 
     public int coins = 0;
@@ -43,7 +37,6 @@ public class Movement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical"); //comando que apanha o Input vertical (W e S)
         float horizontal = Input.GetAxisRaw("Horizontal"); //comando que apanha o Input horizontal (A e D)
         Vector3 direction = new Vector3(-vertical, 0f, horizontal).normalized; //indica a direção da entidade do jogador
-        direction.y = directionY;
 
         //faz o movimento e rotação do jogador
         if (direction.magnitude >= 0.1f)
@@ -54,14 +47,7 @@ public class Movement : MonoBehaviour
             controller.Move(direction * speed * Time.deltaTime);
         }
 
-        //salto
-        if (Input.GetButtonDown("Jump") && controller.isGrounded) //aplica a velocidade de salto quando o botão correspondente a salto (Barra de espaço) é premido
-        {
-            directionY = jumpSpeed;
-        }
-
-        directionY -= gravity * Time.deltaTime; //aplica a força de gravidade à entidade uma vez no ar
-
+        
         if(fov.isInFOV == true)
         {
             gameOver.SetActive(true);
