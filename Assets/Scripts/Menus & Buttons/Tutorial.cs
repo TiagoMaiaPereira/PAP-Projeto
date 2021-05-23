e7adc0a80b8987ed2baf218558a8d7790386aaf7
Tiago Pereira <tiagomaiapereira900@gmail.com>
2021-05-23 18:19:37 +0100

    Adicionei os modelos para o 1º nível e comecei o seu desenvolvimento. Comecei o desenvolvimento do sistema de tutorial.

---
+++ Assets/Scripts/Menus & Buttons/Tutorial.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public Collider tutorial;
+
+    public GameObject tutorialPrompt;
+
+    void OnTriggerEnter(Collider tutorial)
+    {
+        tutorialPrompt.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+}
