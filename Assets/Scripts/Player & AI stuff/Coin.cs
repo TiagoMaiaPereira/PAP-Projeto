74f7260ba80649b9e9bad84dc76991a29ea5fcb0
Tiago Pereira <tiagomaiapereira900@gmail.com>
2021-05-24 11:18:59 +0100

    Consegui fazer um tutorial funcional e fiz progresso no desenvolviento do nível

--- Assets/Scripts/Player & AI stuff/Coin.cs
+++ Assets/Scripts/Player & AI stuff/Coin.cs
@@ -6,6 +6,16 @@ public class Coin : MonoBehaviour
 {
     public Collider coin;
 
+    public Transform coinTrans;
+
+    void Update()
+    {
+        if (transform.position.y <= 1.5f || transform.position.y >= 1.5f)
+        {
+            coinTrans.position = new Vector3 (transform.position.x, 1.5f, transform.position.z);
+        }
+    }
+
     public void OnCollisionEnter(Collision coin)
     {
         Debug.Log("Hit coin");
