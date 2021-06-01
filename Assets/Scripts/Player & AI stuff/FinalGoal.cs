68f7292d6255c8fd1c09fa7e26f529d671d9f313
Tiago Pereira <tiagomaiapereira900@gmail.com>
2021-06-01 12:23:42 +0100

    Terminei o nível 4. NOTA: Existe conflito no sistema de deteção quando há mais do que 2 guardas.

---
+++ Assets/Scripts/Player & AI stuff/FinalGoal.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalGoal : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider collision)
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
